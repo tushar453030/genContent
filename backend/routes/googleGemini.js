3848f7a4a5826b1ed07293393818296642f8bea7
Tushar Sharma <tushar4530@gmail.com>
2024-08-20 23:31:37 +0530

    deepgram

--- backend/routes/googleGemini.js
+++ backend/routes/googleGemini.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { GoogleGenerativeAI } from '@google/generative-ai'
 import dotenv from 'dotenv'
 import { YoutubeTranscript } from 'youtube-transcript'
+import { deepgram, YD } from './deepgramConfig.js'
 
 dotenv.config()
 
@@ -10,6 +11,12 @@ const router = express.Router()
 // Configure Google Generative AI
 const gemini_api_key = process.env.GEMINI_API_KEY
 const googleAI = new GoogleGenerativeAI(gemini_api_key)
+async function getYouTubeVideoId(url) {
+  const regex =
+    /(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
+  const match = url.match(regex)
+  return match ? match[1] : null
+}
 
 const geminiConfig = {
   temperature: 0.9,
@@ -28,7 +35,12 @@ router.post('/LinkedInPost', async (req, res) => {
   try {
     const { videoUrl } = req.body
     console.log(videoUrl)
-    const transcriptArray = await YoutubeTranscript.fetchTranscript(videoUrl)
+    const transcriptArray = await YoutubeTranscript.fetchTranscript(videoUrl, {
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+      },
+    })
 
     const transcriptText = transcriptArray.map((part) => part.text).join(' ')
     console.log(transcriptText)
@@ -50,6 +62,31 @@ router.post('/LinkedInPost', async (req, res) => {
   }
 })
 
+router.post('/LinkedInPostV2', async (req, res) => {
+  try {
+    const { videoUrl } = req.body
+    console.log(videoUrl)
+    const videoId = await getYouTubeVideoId(videoUrl)
+    console.log(videoId)
+    // YD.download(videoId, `${videoId}.mp3`)
+    const song = await YD.downloadSong(videoUrl)
+    console.log(song)
+    // YD.on('progress', (data) => {
+    //   console.log(data.progress.percentage + '% downloaded')
+    // })
+
+    // YD.on('finished', async (err, video) => {
+    //   const videoFileName = video.file
+    //   console.log(`Downloaded ${videoFileName}`)
+    //   console.log(video)
+
+    //   // Continue on to get transcript here
+    // })
+  } catch (error) {
+    console.error('Error generating LinkedIn post:', error)
+    res.status(500).send({ response: 'Error generating LinkedIn post', error })
+  }
+})
 router.post('/BlogPost', async (req, res) => {
   try {
     const { videoUrl } = req.body
